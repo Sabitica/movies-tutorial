3f13bc07f280d8c8fc37c6b05b7bc9604243965d
Sabitica <sarahlecornec123+1@gmail.com>
2024-09-14 09:51:49 +0100

    twig setup

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,19 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
-    // defaults allows a default route parameter 
-    // $name will work so long as it matches the {name} slug
-    #[Route('/movies/{name}', name: 'app_movies', defaults:['name' => null], methods:['GET', 'HEAD'])]
-    public function index($name): JsonResponse
+    // // defaults allows a default route parameter 
+    // // $name will work so long as it matches the {name} slug
+    // #[Route('/movies/{name}', name: 'app_movies', defaults:['name' => null], methods:['GET', 'HEAD'])]
+    // public function index($name): JsonResponse
+    // {
+    //     return $this->json([
+    //         'message' => $name,
+    //         'path' => 'src/Controller/MoviesController.php',
+    //     ]);
+    // }
+
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => $name,
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
+
+        return $this->render('index.html.twig');
     }
+
+
 }
